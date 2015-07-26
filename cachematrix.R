46bb61cfe5d79369bf3abfb6b29aba944559c744
leunghoi <hoifaileung@hushmail.com>
2015-07-26 16:23:41 -0400

    add comments.  test to see if 2 matrices are the same

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script contains 2 key functions
+## makeCacheMatrix: returns a list object that store the inverse of a matrix
+## cacheSolve: returns the inverse of a matrix. The inverse could come from
+##          cache or calcuated.
 
-## Write a short comment describing this function
-
+## create a few example matrices for testing
 x1 <- 1:4
 mx1 <- matrix(x, 2)
 solve(mx1)
@@ -15,29 +16,47 @@ x3 <- 3:6
 mx3 <- matrix(x, 2)
 solve(mx3)
 
+x4 <- 1:4
+mx4 <- matrix(x, 2)
+solve(mx4)
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
-    ## testing git
+    #initialize the inverse
     inverse <- NULL
+    
+    #set the matrix x to be used to calculate the inverse
+    #everytime x is reset, the inverse is reset to null
     set <- function(y) {
-        x <<- y
-        inverse <<- NULL
+        #if every element of y is the same as every elemnt of x, 
+        #there is no need to initialize or set the inverse to null.
+        if(!( dim(x)==dim(y) && all(x==y) )) {
+            x <<- y
+            inverse <<- NULL
+        } else {
+            message("all elements in the matrix are the same")
+        }
     }
+    
+    #return the input matrix
     get <- function() x
+    
+    #cache the inverse
     setinverse <- function(inv) inverse <<- inv
+    
+    #return the inverse
     getinverse <- function() inverse
+    
+    #return the list object
     list(set = set, get = get, 
          setinverse = setinverse,
          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-cm <- makeCacheMatrix(mx1)
-cm$get()
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
     inv <- x$getinverse()
     if(!is.null(inv)) {
         message("getting cached data")
@@ -49,4 +68,11 @@ cacheSolve <- function(x, ...) {
     inv
 }
 
+
+## create test object
+cm <- makeCacheMatrix(mx1)
+cm$get()
+## test to see if the function compares 2 matrices correctly
+cm$set(mx4)
+## calculate the inverse of x
 cacheSolve(cm)
